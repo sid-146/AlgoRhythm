4519272f422c234d38de06c2c2f8710d9002dd32
Sudhanwa Kaveeshwar <sudhanwa.kaveeshwar@gamil.com>
2022-10-23 11:32:52 +0530

    Modified sorts.py

--- Src/python/sorts.py
+++ Src/python/sorts.py
@@ -2,3 +2,15 @@ import helper
 
 def bubble_sort():
     return True
+
+def selectioN_sort():
+    return True
+
+def quick_sort():
+    return True
+
+def insertion_sort():
+    return True
+
+def exponential_sort():
+    return True
