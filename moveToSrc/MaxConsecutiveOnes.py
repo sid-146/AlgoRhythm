4d1dba3fa1034f42d36bc66fcfd2b5d68ec6600e
Sudhanwa Kaveeshwar <sudhanwa.kaveeshwar@gamil.com>
2024-04-07 11:57:22 +0530

    Added new test case

--- moveToSrc/MaxConsecutiveOnes.py
+++ moveToSrc/MaxConsecutiveOnes.py
@@ -5,6 +5,8 @@ Input: nums = [1,1,0,1,1,1]
 Output: 3
 """
 
+# nums = [1,1,1,0,0,1,1,1,0,1,0,1,0,1,1,1,1,1,1,1,1]
+
 def maxConsecutive(nums: list) -> int:
     high = 0
     counter = 0
@@ -14,11 +16,12 @@ def maxConsecutive(nums: list) -> int:
     if len(nums) < 1:
         return
 
+    # [1,1,0,1,1,1]
     for num in nums:
         if num == 0:
             counter = 0
         elif num == 1:
-            counter += 1
+            counter = counter + 1
             if counter > high:
                 high = counter
         else:
@@ -29,9 +32,11 @@ def maxConsecutive(nums: list) -> int:
 if __name__ == '__main__':
     test1 = [1,1,0,1,1,1]
     test2 = [1,0,1,1,0,1]
-
+    test3 = [1,1,1,0,0,1,1,1,0,1,0,1,0,1,1,1,1,1,1,1,1]
 
     assert maxConsecutive(test1) == 3
     print('Test 1 passed')
     assert maxConsecutive(test2) == 2
     print('Test 2 passed')
+    assert maxConsecutive(test3) == 8
+    print('Test 3 passed')
