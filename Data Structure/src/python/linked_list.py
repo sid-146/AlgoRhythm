3b31166d26a3382f0cd6e1d7dc13d2fe1c0ab7a4
Sudhanwa Kaveeshwar <sudhanwa.kaveeshwar@gamil.com>
2025-03-05 02:16:01 +0530

    Updated

--- Data Structure/src/python/linked_list.py
+++ Data Structure/src/python/linked_list.py
@@ -1,80 +1,111 @@
-from utils.utils import Node
 from pydantic import BaseModel
+from typing import Any
 
 
-class LinkedList(BaseModel):
-    # def __init__(self) -> None:
-    try:
-        head: Node | None = None
-        size: int = 0
-        indexHash: dict | None = {"None": False}
-        maxIndex: int = 0
-    except Exception as e:
-        print(f"Error while initializing class variable : {e}")
+class Node(BaseModel):
+    data:Any = None
+    next:Node = None    
 
-    def __isEmpty(self) -> bool:
-        return self.head is None
 
-    def __isPresent(self) -> bool: ...
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.size = 0
 
-    @staticmethod
-    def genIndex(data, size):
-        return f"{data}_{size}"
+    def __is_empty(self):
+        if self.head is None:
+            return True
+        return False
 
-    def addNode(self, data) -> bool:
-        try:
-            new = Node(**data)
-            new.setNext(self.head)
-            self.head = new
-            self.size += 1
-            self.maxIndex = 0
-        except Exception as e:
-            print(f"Error while adding node : {e}")
-            return False
-        # self.indexHash.update({self.genIndex(data, self.size) :  'None'})
+    def add_node(self, data):
+        """
+        Create new node
+        check if head is empty
+            if empty set new node as head of linked list
+            else set next of new node to head and set new node as head of linked list
+        """
+        new_node = Node(data=data)
+        if self.__is_empty():
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
         return True
 
-    def updateNode(self, index: int, update: str): ...
 
-    def show(self):
+    def add_node_at_end(self, data):
+        """
+        Add node at end of linked list 
+
+        approach:
+        - create new node
+        - check if head is none
+            - if none set new node as head
+            - else iterate over linked list till last node
+            - set next of last node to new node and set new node next as none
+        """
+
+        new_node = Node(data= data)
+        if self.__is_empty():
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+
+            current.next = new_node 
+            new_node.next = None
+        
+        return True
+
+
+    def add_node_at_position(self, data, position):
+        """
+        Add node at given position
+        
+        approach:
+        - create new node
+        - check if head is none
+        """
+        return
+
+
+
+    def print_ll(self):
+        """
+        print all nodes of linked list
+        
+        Approach:
+        - store head in a variable
+        - iterate over linked list current is none
+        - print data of current node
+        """
+
         current = self.head
-        try:
-            while current is not None:
-                data = current.getData()
-                print(f"|| {data} ||")
-                print(f"\t||\t\t")
-                current = current.getNext()
-        except Exception as e:
-            print(f"Failed to show linked list : {e}")
+        while current:
+            print(current.data, end="->")
+            current = current.next
+        
+        print()
 
-    # def getSize(self) -> int:
-    #     current = self.head
-    #     size = 0
-    #     while current is not None:
-    #         size += 1
-    #         current = current.getNext()
-
-    #     self.size = size
-    #     return self.size
-
-    def deleteNode(self): ...
 
 
 if __name__ == "__main__":
-    obj = LinkedList()
-    while True:
-        print("1: Add || 2:Print ||0 to exit")
-        a = int(input("Enter option"))
-        if a == 1:
-            res = obj.addNode(data={"data": "Data data for node one"})
+    ll = LinkedList()
 
-            if res:
-                print("Node added")
-            else:
-                print("Failed to add node")
-        elif a == 2:
-            obj.show()
-        elif a == 0:
-            break
-        else:
-            print("Wrong Choice")
+    print("Checking if linked list is empty")
+    assert ll.head is None
+
+    print('Adding new nodes')
+    assert ll.add_node(1) == True 
+    assert ll.add_node(2) == True 
+    assert ll.add_node(3) == True 
+
+
+    ll.print_ll()
+    
+    print('Adding new node at end')
+    assert ll.add_node_at_end(4) == True
+    
+    ll.print_ll()
