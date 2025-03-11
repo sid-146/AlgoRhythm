f094df97f6e8157afdddef68e21fc8ae7dfd2d61
Sudhanwa Kaveeshwar <sudhanwa.kaveeshwar@gamil.com>
2025-03-12 00:35:00 +0530

    updated.

--- PracticeQuestions/src/python/DuplicateZero.py
+++ PracticeQuestions/src/python/DuplicateZero.py
@@ -1,6 +1,6 @@
 """
-Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.  
-Note that elements beyond the length of the original array are not written. 
+Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
+Note that elements beyond the length of the original array are not written.
 Do the above modifications to the input array in place and do not return anything.
 """
 
@@ -15,14 +15,36 @@ Input: arr = [1,2,3]
 Output: [1,2,3]
 Explanation: After calling your function, the input array is modified to: [1,2,3]
 """
-
-def duplicateZero(nums:list):
-    ...
-    
+import random
+from utils.helper import timeit
 
 
+@timeit
+def duplicateZeroBruteForce(nums: list[int]) -> None:
+    """
+    Array is fixed length.
+
+    Steps:
+    1. Iterate over all elements
+    2. If element is 0 then add 0 on next index and move each element
+    3. If element is not 0 then move on to next element
+    """
+    _len = len(nums)
+    for i in range(_len):
+        if nums[i] != 0:
+            continue
+        else:  # means element is 0
+            while i <= _len:
+                j = i + 1
+                holder = nums[j]
+                nums[j] = 0
+                
+
+
+@timeit
 def withoutUsingBuiltIn():
     return
+
 
 if __name__ == "__main__":
     ...
