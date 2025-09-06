f1654ca787f8320ea5e8d00207dfb855311001f3
Sudhanwa <sudhanwa.kaveeshwar@gmail.com>
2025-09-06 21:40:10 +0530

    post order recursive

---
+++ Data Structure/src/python/bst/post_order_recursive.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, value: int):
+        self.left: "Node" = None
+        self.right: "Node" = None
+        self.value: int = value
+
+
+def pre_order(root: Node):
+    # left, right, root
+
+    if root is None:
+        return
+
+    pre_order(root.left)
+    pre_order(root.right)
+    print(root.value, end=" ")
+
+
+def main():
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.left.right = Node(5)
+    root.right.left = Node(6)
+    root.right.right = Node(7)
+
+    pre_order(root)
+
+    # print_tree_pyramid(root)
+
+
+main()
