66527e7b24ff939764c19dc0f3cc5ca658b50423
Sudhanwa Kaveeshwar <65759652+sid-146@users.noreply.github.com>
2025-10-20 18:20:26 +0530

    finalValueAfterOperations.py created file

---
+++ PracticeQuestions/LeetCode/python/finalValueAfterOperations.py
@@ -0,0 +1,8 @@
+def finalValueAfterOperations(self, operations: List[str]) -> int:
+  X = 0
+  for operation in operations:
+    if '++' in operation:
+      X += 1
+    elif '--' in operation:
+      X -= 1
+  return X
